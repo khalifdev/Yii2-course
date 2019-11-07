00cc54a1ccc262694b484126b6a344ae4a2497ff
khalifdev <nur.khalif@yandex.ru>
2019-11-07 17:57:48 +0300

    Сделал наследование SiteController от BaseController

--- controllers/actions/activity/CreateAction.php
+++ controllers/actions/activity/CreateAction.php
@@ -14,9 +14,6 @@ class CreateAction extends BaseAction
     {
         $model = \Yii::$app->activity->getModel();
 
-//        \Yii::$app->session->set('sdf','val');
-//        \Yii::$app->session->get('sdf','val');
-
         if (\Yii::$app->request->isPost) {
             $model->load(\Yii::$app->request->post());
             if (\Yii::$app->activity->addActivity($model)) {
